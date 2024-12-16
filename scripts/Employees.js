94055819e3a137bda54f351e0ce5c92251026073
Nate Z <ntzeitlin@gmail.com>
2024-12-16 14:37:19 -0600

    added office locations

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -3,12 +3,33 @@
 import { getData } from "./Data.js"
 
 export const generateEmployees = async () => {
-    const employees = await getData("employees?_expand=computer")
+    const employees = await getData("employees?_expand=computer&_expand=department&_expand=location")
 
-    let employeeListHTML = "<ul>"
+    let employeeListHTML = ""
 
-    employeeListHTML += employees.map(({ id: id, firstName: firstN, lastName: lastN, computer: { model: model, year: year } }) => `
-    <div><li>${firstN} ${lastN} uses a ${year} ${model}</li></div>
-    ` ).join("")
+    employeeListHTML += employees.map(
+        ({ id: employeeId, firstName: firstN, lastName: lastN,
+            computer: { model: model, year: year },
+            department: { name: departmentName },
+            location: { city: officeCity }
+        }) =>
+            `
+    <div class="employee">
+        <header class="employee__name"> 
+            <h1>${firstN} ${lastN}</h1>
+        </header>
+        <section class="employee__computer">
+            Currently using a ${year} ${model}
+        </section>
+        <section class="employee__department">
+            Works in ${departmentName} department
+        </section>
+        <section class="emoployee__location">
+            Works at the ${officeCity} office
+        </section>
+    </div>
+
+    `).join("")
+
     return employeeListHTML
 }    
